db8ec9bb0b2bc6177f0601b8e2d780d9a7efa5f8
Bexzod Abduvaliev <bexzodabduvaliev@gmail.com>
2020-04-08 11:55:12 -0400

    fixed window size

--- src/GoHomeBoardGame.java
+++ src/GoHomeBoardGame.java
@@ -5,7 +5,7 @@ public class GoHomeBoardGame {
 	public static void main(String[] args) {
 		try {
 			SwingUtilities.invokeLater(()->{
-				new Window("GoHomeBoardGame", 1294, 720);
+				new Window("GoHomeBoardGame", 1295, 720);
 			});
 		} catch (Exception e) {
 			e.printStackTrace();
